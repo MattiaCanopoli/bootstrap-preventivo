afd3687aea052a9c1b6f3e8c6a9bc0af11b305ad
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-05-27 15:12:54 +0200

    feat: jobTime and jobCost variables are now determined dynamically

--- main.js
+++ main.js
@@ -1,40 +1,41 @@
 'use strict'
 //prendo elemento select dal DOM
-const job = document.getElementById('form-job')
+const jobType = document.getElementById('form-job')
 
-//creo variabili costo orario per ogni job
-const backCost = 20.5
-const frontCost = 15.3
-const projectCost = 33.6
+let jobCost // creo una variabile jobCost per determinare il costo orario a seconda del valore di job
+let jobTime // creo una variabile jobTime per determinare il tempo di lavoro a seconda del valore di job
 
-//creo variabili tempo espresso in ore per ogni job
-const backTime = 15
-const frontTime = 12
-const projectTime = 20
+//entrambe le variabili cambieranno valore al cambiare della selezione del tipo di lavoro
+jobType.addEventListener('change', function () {
 
-//prendo elemento submit button dal DOM
-const subBtn = document.getElementById('subBtn')
+    if (jobType.value === 'back') {
+        jobCost = 20.5
+        jobTime = 15
+    } else if (jobType.value === 'front') {
+        jobCost = 15.3
+        jobTime = 12
+    } else if (jobType.value === 'project') {
+        jobCost = 33.6
+        jobTime = 20
+    }
+})
 
-//prendo elemento span con prezzo finale dal DOM
-const priceDiv = document.getElementById('price-div')
-const finalPrice = document.getElementById('final-price')
+const subBtn = document.getElementById('subBtn') //prendo elemento submit button dal DOM
+const priceDiv = document.getElementById('price-div') //prendo div che contine la sezione prezzo dal DOM
+const finalPrice = document.getElementById('final-price') //prendo elemento span con prezzo finale dal DOM
 
 subBtn.addEventListener('click', function (event) {
-    event.preventDefault()
+    event.preventDefault() //blocco l'esecuzione default del submit per il prompt
 
-    let jobPrice
+    /*
+    inserire validazione qui
+    */
 
-    if (job.value === 'back') {
-        jobPrice = backCost * backTime
-    } else if (job.value === 'front') {
-        jobPrice = frontCost * frontTime
-    } else if (job.value === 'project') {
-        jobPrice = projectCost * projectTime
-    }
+    let jobPrice = jobCost * jobTime //calcolo il prezzo del lavoro al lordo di sconti
 
     console.log(jobPrice.toFixed(2))
 
-    finalPrice.innerText = jobPrice.toFixed(2)
-    priceDiv.classList.remove('d-none')
+    finalPrice.innerText = jobPrice.toFixed(2) //stampo in pagina il prezzo jobPrice
+    priceDiv.classList.remove('d-none') //rimuovo la classe d-none per mostrare l'elemento in pagina
 
 })
